38a2ebeb3ff5a7069e54740441e2dfce78a98c68
ingerstep <stepan.karelin.1996@mail.ru>
2024-12-05 08:41:41 +0700

    feat(db): add seed, docker compose, prisma client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { db } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await db.game.findMany();
+
   return (
     <div>
       <Button>Hello</Button>
+      {games.map((card) => (
+        <Card key={card.id}>
+          <CardTitle>{card.name}</CardTitle>
+        </Card>
+      ))}
     </div>
   );
 }
